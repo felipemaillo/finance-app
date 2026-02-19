4621ca7906770248fbb5bde8039dc1405a81dd94
felipemaillo <felipemaillo@icloud.com>
2026-02-19 14:49:52 +0100

    Show port

--- backend/index.js
+++ backend/index.js
@@ -274,5 +274,5 @@ app.get('/currencies', async (req, res) => {
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`🚀 Servidor rodando`);
+  console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
